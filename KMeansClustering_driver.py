b751c06138b66942086cde58a647f5e75c345fa1
ethan05 <60231018+ethan05@users.noreply.github.com>
2020-04-14 19:49:59 -0400

    Update KMeansClustering_driver.py

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -1,2 +1,30 @@
-#Please place your FUNCTION code for step 4 here.
-import KMeansClustering_functions as kmc #Use kmc to call your functions
+#Project 2
+#Step 4 Driver 
+#by Ethan Donnelly
+#Individual work, spent about 30 minutes on this code.
+
+
+from part4functions import *
+import matplotlib.pyplot as plt
+
+glucose, hemoglobin, classification = openckdfile()
+
+#k values were changed for different graphs
+k=2
+m=10
+
+centroids = select(k)
+assignments = assign(centroids, hemoglobin, glucose)
+final_a, final_p = iterate(k,hemoglobin,glucose,m)
+
+point=(rescale(k,final_p))
+print(final_a)
+print(classification)
+plt.figure()
+plt.plot((hemoglobin[classification==1]),(glucose[classification==1]), "k.", label = "Class 1")
+plt.plot((hemoglobin[classification==0]),(glucose[classification==0]), "r.", label = "Class 0")
+plt.scatter(point[0::2],point[1::2], s=100)
+plt.xlabel("Hemoglobin")
+plt.ylabel("Glucose")
+plt.legend()
+plt.show()
